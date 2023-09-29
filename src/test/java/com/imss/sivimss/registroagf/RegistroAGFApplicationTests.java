0df25812782c5e4b9e7f0b28f213c2f2d17005a0
METRO\raul.arteaga <raul.arteaga@people-media.com.mx>
2023-09-29 15:26:13 -0600

    Se remueve configuracion de test

--- src/test/java/com/imss/sivimss/registroagf/RegistroAGFApplicationTests.java
+++ src/test/java/com/imss/sivimss/registroagf/RegistroAGFApplicationTests.java
@@ -1,16 +1,10 @@
 package com.imss.sivimss.registroagf;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import com.imss.sivimss.registroagf.RegistroAGFApplication;
-
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
 class RegistroAGFApplicationTests {
 
-	@Test
+	
 	void contextLoads() {
 		String result="test";
 		RegistroAGFApplication.main(new String[]{});
